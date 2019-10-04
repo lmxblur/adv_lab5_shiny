a68664382837cd911de49c28b84188773160f9b2
spirosdim <spirosdmtr@gmail.com>
2019-10-04 22:55:36 +0200

    remove unused method

--- app.R
+++ app.R
@@ -41,18 +41,6 @@ server <- function(input, output) {
                             id <- mlist$id[mlist$municipality==Mname]
                             return(id)
                           },
-                          get_skola = function(municipal){
-                            'Get the school list in a municipality'
-                            path_sk <- "http://api.kolada.se/v2/ou?"
-                            id <- get_id(municipal)
-                            api_raw_ret <- httr::GET(url = path_sk,
-                                                     query = list(municipality=id,title="skola"))
-                            api_text <- httr::content(api_raw_ret, as = "text", encoding = "UTF-8")
-                            skola_json <- data.frame(jsonlite::fromJSON(api_text,flatten = TRUE))
-                            sk_list <- skola_json['values.title']
-                            colnames(sk_list) <- c('school')
-                            return(sk_list)
-                          },
                           get_numb = function(Mname, op){
                             'Get the statistics'
                             #1 dead
